caac4f5a9d0b7482523b1e8b04b3570e792c8135
Agahnim <gabrielguysoulard@gmail.com>
2026-01-09 14:02:43 +0100

    update flake

--- flake.nix
+++ flake.nix
@@ -1,92 +1,42 @@
 {
+  description = "Tauri devenv";
+
   inputs = {
-    nixpkgs.url = "nixpkgs";
-    flake-utils.url = "github:numtide/flake-utils";
-    rust-overlay.url = "github:oxalica/rust-overlay";
+    nixpkgs.url = "github:NixOS/nixpkgs";
   };
 
   outputs = {
     self,
     nixpkgs,
-    flake-utils,
-    rust-overlay,
-  }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = import nixpkgs {
-        inherit system;
-        overlays = [rust-overlay.overlays.default];
-      };
-
-      toolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
-
-      packages = with pkgs; [
-        cargo
-
-        cargo-tauri
-
-        toolchain
-
-        rust-analyzer-unwrapped
-
-        nodejs_18
-
-        nodePackages.pnpm
-      ];
-
-      nativeBuildPackages = with pkgs; [
+  }: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {inherit system;};
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      nativeBuildInputs = with pkgs; [
         pkg-config
-
-        dbus
-
-        openssl
-
-        glib
-
-        gtk3
-
-        libsoup
-
-        webkitgtk
-
-        librsvg
+        gobject-introspection
+        cargo
+        bun
+        cargo-tauri
+        nodejs
+        rustc
       ];
 
-      libraries = with pkgs; [
-        webkitgtk
-
-        gtk3
-
+      buildInputs = with pkgs; [
+        at-spi2-atk
+        atkmm
         cairo
-
         gdk-pixbuf
-
         glib
-
-        dbus
-
-        openssl
-
+        gtk3
+        harfbuzz
         librsvg
+        libsoup_3
+        pango
+        webkitgtk_4_1
+        openssl
       ];
-    in {
-      devShells.default = pkgs.mkShell {
-        buildInputs = packages;
-
-        nativeBuildInputs = nativeBuildPackages;
-
-        shellHook = with pkgs; ''
-          export LD_LIBRARY_PATH="${
-            lib.makeLibraryPath libraries
-          }:$LD_LIBRARY_PATH"
-
-          export OPENSSL_INCLUDE_DIR="${openssl.dev}/include/openssl"
-
-          export OPENSSL_LIB_DIR="${openssl.out}/lib"
-
-          export OPENSSL_ROOT_DIR="${openssl.out}"
-
-          export RUST_SRC_PATH="${toolchain}/lib/rustlib/src/rust/library"
-        '';
-      };
-    });
+    };
+  };
 }
