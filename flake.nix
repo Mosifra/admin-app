4f9e8ed3c0a2491b7ea9f024b762d253f0e1f954
Agahnim <gabrielguysoulard@gmail.com>
2026-01-15 15:22:52 +0100

    update flake

--- flake.nix
+++ flake.nix
@@ -48,6 +48,7 @@
         mkdir -p $HOME/.local/bin
         ln -sf $(which xdg-open) $HOME/.local/bin/xdg-open
         export PATH=$HOME/.local/bin:$PATH
+        bun tauri dev &
         neovide &
       '';
     };
