8140dbe50ba631857c05c822a6cec5fc8c48fe09
Agahnim <gabrielguysoulard@gmail.com>
2026-01-19 12:39:06 +0100

    fix route for companies

--- src-tauri/src/api.rs
+++ src-tauri/src/api.rs
@@ -1,87 +1,87 @@
 use crate::commands::get_base_url;
 use crate::domain::{
-	CreateCompanyResponse, CreateUniversityResponse, FetchCompaniesResponse,
-	FetchUniversitiesResponse,
+    CreateCompanyResponse, CreateUniversityResponse, FetchCompaniesResponse,
+    FetchUniversitiesResponse,
 };
 use std::collections::HashMap;
 
 pub async fn create_university_request(
-	login: String,
-	mail: String,
-	name: String,
-	jwt: String,
+    login: String,
+    mail: String,
+    name: String,
+    jwt: String,
 ) -> Result<CreateUniversityResponse, reqwest::Error> {
-	let mut body = HashMap::new();
-	body.insert("login", login);
-	body.insert("mail", mail);
-	body.insert("name", name);
-	let mut url = get_base_url().await.unwrap();
-	url.push_str("/create/university");
-	let client = reqwest::Client::new();
-	let body = client
-		.post(url)
-		.bearer_auth(jwt)
-		.json(&body)
-		.send()
-		.await?
-		.json()
-		.await?;
-	Ok(body)
+    let mut body = HashMap::new();
+    body.insert("login", login);
+    body.insert("mail", mail);
+    body.insert("name", name);
+    let mut url = get_base_url().await.unwrap();
+    url.push_str("/create/university");
+    let client = reqwest::Client::new();
+    let body = client
+        .post(url)
+        .bearer_auth(jwt)
+        .json(&body)
+        .send()
+        .await?
+        .json()
+        .await?;
+    Ok(body)
 }
 
 pub async fn create_company_request(
-	login: String,
-	mail: String,
-	name: String,
-	jwt: String,
+    login: String,
+    mail: String,
+    name: String,
+    jwt: String,
 ) -> Result<CreateCompanyResponse, reqwest::Error> {
-	let mut body = HashMap::new();
-	body.insert("login", login);
-	body.insert("mail", mail);
-	body.insert("name", name);
-	let mut url = get_base_url().await.unwrap();
-	url.push_str("/create/company");
-	let client = reqwest::Client::new();
-	let body = client
-		.post(url)
-		.bearer_auth(jwt)
-		.json(&body)
-		.send()
-		.await?
-		.json()
-		.await?;
-	Ok(body)
+    let mut body = HashMap::new();
+    body.insert("login", login);
+    body.insert("mail", mail);
+    body.insert("name", name);
+    let mut url = get_base_url().await.unwrap();
+    url.push_str("/create/companies");
+    let client = reqwest::Client::new();
+    let body = client
+        .post(url)
+        .bearer_auth(jwt)
+        .json(&body)
+        .send()
+        .await?
+        .json()
+        .await?;
+    Ok(body)
 }
 
 pub async fn fetch_universities_request(
-	jwt: String,
+    jwt: String,
 ) -> Result<FetchUniversitiesResponse, reqwest::Error> {
-	let mut url = get_base_url().await.unwrap();
-	url.push_str("/user/universities");
-	let client = reqwest::Client::new();
-	let body = client
-		.get(url)
-		.bearer_auth(jwt)
-		.send()
-		.await?
-		.json()
-		.await?;
-	println!("{body:?}");
-	Ok(body)
+    let mut url = get_base_url().await.unwrap();
+    url.push_str("/user/universities");
+    let client = reqwest::Client::new();
+    let body = client
+        .get(url)
+        .bearer_auth(jwt)
+        .send()
+        .await?
+        .json()
+        .await?;
+    println!("{body:?}");
+    Ok(body)
 }
 
 pub async fn fetch_companies_request(
-	jwt: String,
+    jwt: String,
 ) -> Result<FetchCompaniesResponse, reqwest::Error> {
-	let mut url = get_base_url().await.unwrap();
-	url.push_str("/user/company");
-	let client = reqwest::Client::new();
-	let body = client
-		.get(url)
-		.bearer_auth(jwt)
-		.send()
-		.await?
-		.json()
-		.await?;
-	Ok(body)
+    let mut url = get_base_url().await.unwrap();
+    url.push_str("/user/company");
+    let client = reqwest::Client::new();
+    let body = client
+        .get(url)
+        .bearer_auth(jwt)
+        .send()
+        .await?
+        .json()
+        .await?;
+    Ok(body)
 }
