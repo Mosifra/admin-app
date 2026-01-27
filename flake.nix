723daf444b6d5723a99507ed9a674581a6edc44e
Agahnim <gabrielguysoulard@gmail.com>
2026-01-27 11:27:56 +0100

    refacto flake to use tmux

--- flake.nix
+++ flake.nix
@@ -13,30 +13,22 @@
     packages.${system}.default = pkgs.rustPlatform.buildRustPackage rec {
       pname = "mosifra-admin-app";
       version = "0.1.0";
-
       src = ./.;
-
       cargoHash = "sha256-h6yp64aQpj534dnxytsnmPdbAPp0QGjCQ20xeDgIlRg=";
-
       npmDeps = pkgs.fetchNpmDeps {
         name = "${pname}-${version}-npm-deps";
         inherit src;
         hash = "sha256-jE58v93Jfxe+kKSXQ+5R0p1VusaSi/SwIwJ+V1gVe+o=";
       };
-
       makeCacheWritable = true;
-
       nativeBuildInputs = with pkgs; [
         cargo-tauri.hook
-
         nodejs
         bun
         pkgs.npmHooks.npmConfigHook
-
         pkg-config
         wrapGAppsHook3
       ];
-
       buildInputs = with pkgs; [
         glib-networking
         openssl
@@ -51,17 +43,14 @@
         libsoup_3
         pango
       ];
-
       cargoRoot = "src-tauri";
       buildAndTestSubdir = cargoRoot;
-
       meta = with pkgs.lib; {
         description = "Mosifra Admin App";
         license = licenses.mit;
         platforms = platforms.linux;
       };
     };
-
     devShells.${system}.default = pkgs.mkShell {
       nativeBuildInputs = with pkgs; [
         pkg-config
@@ -74,6 +63,7 @@
         rustfmt
         clippy
         rust-analyzer
+        tmux
       ];
       buildInputs = with pkgs; [
         at-spi2-atk
@@ -96,11 +86,22 @@
         export XDG_DATA_DIRS=${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}:${pkgs.gtk3}/share/gsettings-schemas/${pkgs.gtk3.name}:$XDG_DATA_DIRS;
         export GIO_MODULE_DIR="${pkgs.glib-networking}/lib/gio/modules"
         export WEBKIT_DISABLE_COMPOSITING_MODE=1
-
         mkdir -p $HOME/.local/bin
         ln -sf $(which xdg-open) $HOME/.local/bin/xdg-open
         export PATH=$HOME/.local/bin:$PATH
-        neovide &
+
+        SESSION_NAME="tauri-dev"
+
+        if ! tmux has-session -t $SESSION_NAME 2>/dev/null; then
+          tmux new-session -d -s $SESSION_NAME -n "editor"
+          tmux send-keys -t $SESSION_NAME:0 "neovide" C-m
+
+          tmux new-window -t $SESSION_NAME:1 -n "shell"
+
+          tmux select-window -t $SESSION_NAME:1
+        fi
+
+        tmux attach-session -t $SESSION_NAME
       '';
     };
   };
